e8e4670ab4b4d2e574c7cd7854ac32767388fbcc
yifanshi006129 <yifanshi006129@sohu-inc.com>
2018-12-14 17:09:57 +0800

    2018/12/14 commited at sohu

---
+++ src/com/sohu/yifanshi/test/maxCoins/Test.java
@@ -0,0 +1,9 @@
+package com.sohu.yifanshi.test.maxCoins;
+
+public class Test {
+    public static void main(String[] args) {
+        int[] nums = new int[]{7,9,8,0,7,1,3,5,5,2,3};
+
+        System.out.println(Solution.maxCoins(nums));
+    }
+}
