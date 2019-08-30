08fab3dbf2a5e9ce7f8c80ab04b14fcf46d45e2f
254475503@qq.com <254475503@qq.com>
2019-08-30 16:40:55 +0800

    2019/8/29 最大子串记录路径

--- src/main/java/com/sohu/yifanshi/test/maxSubArray/MaxSubArray_dp.java
+++ src/main/java/com/sohu/yifanshi/test/maxSubArray/MaxSubArray_dp.java
@@ -1,5 +1,7 @@
 package com.sohu.yifanshi.test.maxSubArray;
 
+import org.junit.Test;
+
 public class MaxSubArray_dp {
     //状态方程 ： max( dp[ i ] )  = getMax(  max( dp[ i -1 ] ) + arr[ i ] ,arr[ i ] ) 。
     // 上面式子的意义是：我们从头开始遍历数组，遍历到数组元素 arr[ i ] 时，连续的最大的和 可能为 max( dp[ i -1 ] ) + arr[ i ] ，
@@ -9,13 +11,33 @@ public class MaxSubArray_dp {
     public static int maxSubArray(int[] nums) {
         int sum = nums[0];
         int max = nums[0];
+        String str = "";
+        String maxstr="";
         for(int i = 1 ; i < nums.length ; i ++)
         {
-            sum = sum+nums[i]>nums[i]? sum+nums[i] : nums[i];
+            if(sum+nums[i]>nums[i])
+            {
+                sum = sum+nums[i];
+                str+=" "+nums[i];
+            }
+            else {
+                sum = nums[i];
+                str = String.valueOf(nums[i]);
+            }
             if(sum>max)
+            {
                 max = sum;
+                maxstr = str;
+            }
         }
+        System.out.println(maxstr);
         return max;
+    }
+
+    @Test
+    public void test()
+    {
+        System.out.println(MaxSubArray_dp.maxSubArray(new int[]{-2,9,-7,-10,1,1,6,1,-7,1,-3,1,5,-10,12,10,-3,-1,5,7,-2,6,-4,7,7}));
     }
 
 }
