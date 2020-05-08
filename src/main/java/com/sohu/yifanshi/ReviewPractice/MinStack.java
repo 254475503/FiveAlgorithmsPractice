75cb63fe346f7e171f1747d3f30b01f86038eb01
shiyf286 <shiyf286@chinaunicom.cn>
2020-05-08 20:26:40 +0800

    111

---
+++ src/main/java/com/sohu/yifanshi/ReviewPractice/MinStack.java
@@ -0,0 +1,38 @@
+package com.sohu.yifanshi.ReviewPractice;
+
+import java.util.EmptyStackException;
+import java.util.LinkedList;
+import java.util.Stack;
+
+public class MinStack {
+    private Stack<Integer> mainStack;
+    private Stack<Integer> minStack ;
+    LinkedList
+    public MinStack() {
+        mainStack = new Stack();
+        minStack = new Stack();
+    }
+
+    public void push(int x) {
+        mainStack.push(x);
+        if(minStack.size()==0||x<minStack.peek())
+            minStack.push(x);
+    }
+
+    public void pop() {
+        int min = minStack.peek();
+        if(mainStack.peek() == min){
+            minStack.pop();
+        }
+        mainStack.pop();
+
+    }
+
+    public int top() {
+        return mainStack.peek();
+    }
+
+    public int min() {
+        return minStack.peek();
+    }
+}
